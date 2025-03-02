15ce8bbabffadfe374765a73bfe701ccd339ecd9
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-02 18:30:13 +0600

    update for passing token when try to connect with backend hub through signalr

--- frontend/ticketbooking.app/src/services/signalr.service.js
+++ frontend/ticketbooking.app/src/services/signalr.service.js
@@ -1,12 +1,16 @@
 import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 const companyId = "3";
+const token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNyc2Etc2hhMjU2IiwidHlwIjoiSldUIn0.eyJzZXNzaW9uIjoiMTc0ZGE3MzItZjc1Yy0xMWVmLTlkYjUtMzA4NWE5ZWUyNWQ0LWMwOWNhZjZkNGNhNTNjMWJlNmY3ZTlmOWJkYWIwNzEzIiwibmJmIjoxNzQwOTE2MDM3LCJleHAiOjE3NDM1MDgwMzcsImlzcyI6InF1aW5ib29rLmNvbSIsImF1ZCI6InF1aW5ib29rLmNvbSJ9.DcbDmsArrNm1vEfWNz5ArJJFnqI-aBubK1jUxjvv6t9TS0EZRL6PvtzIt7hzu1SHQWrierBHcIXuC4aW62oy32FgFEwKAUhECGseDca5ItUfaeCWG8JtNuL9ThMM-KgqKNVsFm1J0CMHSLnFNpMGri9UEanhY1mhelA3yY9uvYfStqXCM_KcYJv1QCJnbQaMyRkNSOyTyoubUMdbF_hbN3CVbcpG68Cte0vd5XW7KskFxmnClr7qOWwMNuJ960SZVKiGt9PoG3LF4r-mI29uw0R__Q_vx36PyqBuU4KTeqqVVr8lIMDZh3LE4kIuCHa-evKUtIpNIAiICVvHFfYN6w";
 
 class SignalRService {
+  
   constructor() {
     if (!SignalRService.instance) {
       this.connection = new HubConnectionBuilder()
-        .withUrl("https://localhost:7093/ticketHub")
+        .withUrl("http://localhost:5000/ticketHub", {
+          accessTokenFactory: () => token // Attach token to the request
+        })
         .configureLogging(LogLevel.Information)
         .withAutomaticReconnect()
         .build();
